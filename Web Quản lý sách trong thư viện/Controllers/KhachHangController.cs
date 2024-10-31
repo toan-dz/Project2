dea5afb1499c99d568d5255ebed817301cdca83c
toan-dz <ltoan5861@gmail.com>
2024-10-31 09:50:40 +0700

    Web Quản lý sách trong thư viện

---
+++ Web Quản lý sách trong thư viện/Controllers/KhachHangController.cs
@@ -0,0 +1,124 @@
+﻿using Quản_Lý_Sách.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web_Quản_lý_sách_trong_thư_viện.Models;
+
+namespace Web_Quản_lý_sách_trong_thư_viện.Controllers
+{
+    public class KhachHangController : Controller
+    {
+        private LibraryContext db = new LibraryContext();
+
+        // GET: KhachHang
+        public ActionResult Index()
+        {
+            return View(db.KhachHang.ToList());
+        }
+
+        // GET: KhachHang/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHang.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // GET: KhachHang/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KhachHang/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaKH,HoTen,NgaySinh,DiaChi,Email,SoDienThoai")] KhachHang khachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KhachHang.Add(khachHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(khachHang);
+        }
+
+        // GET: KhachHang/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHang.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // POST: KhachHang/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaKH,HoTen,NgaySinh,DiaChi,Email,SoDienThoai")] KhachHang khachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khachHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(khachHang);
+        }
+
+        // GET: KhachHang/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHang.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // POST: KhachHang/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            KhachHang khachHang = db.KhachHang.Find(id);
+            db.KhachHang.Remove(khachHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web Quản lý sách trong thư viện/Controllers/QuanTriController .cs
@@ -0,0 +1,124 @@
+﻿using Quản_Lý_Sách.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web_Quản_lý_sách_trong_thư_viện.Models;
+
+namespace Web_Quản_lý_sách_trong_thư_viện.Controllers
+{
+    public class QuanTriController : Controller
+    {
+        private LibraryContext db = new LibraryContext();
+
+        // GET: QuanTri
+        public ActionResult Index()
+        {
+            return View(db.QuanTri.ToList());
+        }
+
+        // GET: QuanTri/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanTri quanTri = db.QuanTri.Find(id);
+            if (quanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanTri);
+        }
+
+        // GET: QuanTri/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QuanTri/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaQuanTri,TenDangNhap,MatKhau")] QuanTri quanTri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.QuanTri.Add(quanTri);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(quanTri);
+        }
+
+        // GET: QuanTri/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanTri quanTri = db.QuanTri.Find(id);
+            if (quanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanTri);
+        }
+
+        // POST: QuanTri/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaQuanTri,TenDangNhap,MatKhau")] QuanTri quanTri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(quanTri).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(quanTri);
+        }
+
+        // GET: QuanTri/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            QuanTri quanTri = db.QuanTri.Find(id);
+            if (quanTri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(quanTri);
+        }
+
+        // POST: QuanTri/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            QuanTri quanTri = db.QuanTri.Find(id);
+            db.QuanTri.Remove(quanTri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
